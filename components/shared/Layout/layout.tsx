7ffbb492772a162ef8f902683f6c1bbe52e4675d
mujeex <mujahid.bappai@yahoo.com>
2022-11-12 11:20:30 +0100

    style: restyle top navigation

--- components/shared/Layout/layout.tsx
+++ components/shared/Layout/layout.tsx
@@ -3,7 +3,7 @@ import {
     UserOutlined,
     VideoCameraOutlined,
   } from '@ant-design/icons';
-  import {  Menu, Breadcrumb, Typography ,Button, Layout} from 'antd';
+  import {  Menu, Breadcrumb, Typography ,Button, Layout, MenuProps} from 'antd';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { ReactNode, useState } from 'react';
@@ -14,12 +14,19 @@ import OrgSwitcherModal from '../OrgSwitcherModal/OrgSwitcherModal';
 import UnAuthenticatedView from '../UnAuthenticated/UnAuthenticatedView';
 
 
+
 const { Header, Sider, Content } = Layout;
 
   interface LayoutProps{
     children: ReactNode,
     width?: number
   }
+
+  const items1: MenuProps['items'] = ['1', '2', '3'].map(key => ({
+    key,
+    label: `nav ${key}`,
+  }));
+  
   const AppLayout: React.FC<LayoutProps> = ({children,width=80}) => {
     
     const [collapsed, setCollapsed] = useState(false); 
@@ -33,12 +40,51 @@ const { Header, Sider, Content } = Layout;
 
   
     return (
+
       <Layout style={{minHeight:'100vh'}} className=' h-full'>
         <OrgSwitcherModal
           isModalOpen={showSwitcherModal}
           onCloseModal={()=>setSwitcherModal(!showSwitcherModal)}
         />
-        <Sider collapsible onCollapse={() => setCollapsed(!collapsed)} collapsed={collapsed} >
+
+              <Header style={{lineHeight:'1.4',background:'white', width:'100%', display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+              <Menu
+                style={{height:'100%',display:'flex', flex:'3'}}
+                  theme="light"
+                  mode="horizontal"
+                  defaultSelectedKeys={[splittedRoutes[3]]}
+                  items={[
+                    {
+                      key: 'dashboard',
+                      label: <div style={{height:'100%',display:'flex',alignItems:'center'}}> <Link style={{border: '1px solid'}} type='link' href={`/organisation/${orgId}/dashboard`}>Dashboard</Link> </div> ,
+                    },
+                    {
+                      key: 'bookings',
+                      label: <div style={{height:'100%',display:'flex',alignItems:'center'}}> <Link type='link' href={`/organisation/${orgId}/bookings`}>Bookings</Link> </div> ,
+                    },
+                    {
+                      key: 'services',
+                      label: <div style={{height:'100%',display:'flex',alignItems:'center'}}> <Link type='link' href={`/organisation/${orgId}/venues`}>Services</Link> </div> ,
+                    },
+                    {
+                      key: 'staffs',
+                      label: <div style={{height:'100%',display:'flex',alignItems:'center'}}> <Link type='link' href={`/organisation/${splittedRoutes[2]}/staffs`}>Staff</Link> </div> ,
+                    },
+                  ]}
+                />
+                {
+                  !isAuthenticated ? <Button onClick={()=>setIsAuthenticated(true)}>Login</Button>
+                  :(
+                    <div style={{display:'flex',flex:'2'}}>
+                      <OrgSwitcher onOpenSwitcher={()=>setSwitcherModal(!showSwitcherModal)}/>
+                      <CurrentUser user={{email:'mbappai@yahoo.com',role:'admin'}}/>
+                    </div>
+                    )
+                }
+    
+              </Header> 
+        <Layout style={{width:'50%'}} className="site-layout">
+        {/* <Sider collapsible onCollapse={() => setCollapsed(!collapsed)} collapsed={collapsed} >
           <div className="h-6 m-5" />
           <Menu
             theme="dark"
@@ -72,25 +118,10 @@ const { Header, Sider, Content } = Layout;
               }
             ]}
           />
-        </Sider>
-        <Layout style={{width:'50%'}} className="site-layout">
-          <Header style={{lineHeight:'1.4',background:'white', width:'100%', display:'flex', justifyContent:'flex-end', alignItems:'center'}}>
-            {
-              !isAuthenticated ? <Button onClick={()=>setIsAuthenticated(true)}>Login</Button>
-              :(
-                <>
-                  <OrgSwitcher onOpenSwitcher={()=>setSwitcherModal(!showSwitcherModal)}/>
-                  <CurrentUser user={{email:'mbappai@yahoo.com',role:'admin'}}/>
-                </>
-                )
-            }
+        </Sider> */}
 
-          </Header> 
 
-          {/* <Breadcrumb style={{ margin: '16px 2em' }}>
-            <Breadcrumb.Item>Stores</Breadcrumb.Item>
-          </Breadcrumb> */}
-
+{/* 
           <Content
             style={{
               padding: '1em',
@@ -103,7 +134,7 @@ const { Header, Sider, Content } = Layout;
             }}
           >
             {isAuthenticated? children : <UnAuthenticatedView/>}
-          </Content>
+          </Content> */}
         </Layout>
       </Layout>
     );
