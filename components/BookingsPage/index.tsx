589b619e2346a2f3ab217eada4979de4bd2c5e46
mujeex <mujahid.bappai@yahoo.com>
2022-11-13 06:55:16 +0100

    feat: search columns in bookings table

--- components/BookingsPage/index.tsx
+++ components/BookingsPage/index.tsx
@@ -1,8 +1,10 @@
-
-import React from 'react'
-import {Typography,Button,Avatar, Tag} from 'antd'
+import { SearchOutlined } from '@ant-design/icons';
+import type { FilterConfirmProps } from 'antd/es/table/interface';
+import React, { useRef, useState } from 'react'
+import {Typography,Button,Avatar, Tag, InputRef, Input, Space} from 'antd'
 import { useRouter } from 'next/router'
-import Table, { ColumnsType } from 'antd/lib/table';
+import Table, { ColumnsType, ColumnType } from 'antd/lib/table';
+import Highlighter from 'react-highlight-words'
 
 
 
@@ -22,6 +24,8 @@ interface DataType {
     paymentIntentStatus: string,
     orgServiceItemId: string,
   }
+
+  type DataIndex = keyof DataType;
 
 const bookings: DataType[] = [
     {
@@ -71,6 +75,102 @@ export default function Bookings(){
 
   const router = useRouter()
 
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: DataIndex,
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: visible => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: text =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
 
   const columns: ColumnsType<any> = [
     {
@@ -83,6 +183,7 @@ export default function Bookings(){
       title: 'Unique Code',
       dataIndex: 'uniqueCode',
       key: 'uniqueCode',
+      ...getColumnSearchProps('uniqueCode'),
     },
     {
       title: 'Service',
