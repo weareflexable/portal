e6505bf5f6cf75a4a089e242b93d671863ad01f9
mujeex <mujahid.bappai@yahoo.com>
2022-11-14 13:17:22 +0100

    feat: create context for services

---
+++ context/ServicesContext.tsx
@@ -0,0 +1,46 @@
+import React,{useState,useContext,createContext, ReactNode} from 'react';
+import useServices from '../hooks/useServices';
+import {Service} from '../types/Services'
+
+const ServiceContext = createContext<Values|undefined>(undefined);
+
+
+
+type Values = {
+    currentService: Service,
+    switchService: (service:Service)=>void,
+}
+
+interface ServiceContextProviderProps{
+    children: ReactNode
+}
+
+const ServiceContextProvider = ({children}:ServiceContextProviderProps)=>{
+
+
+    const {switchService,currentService} = useServices()
+
+    const values: Values = {
+        currentService,
+        switchService,
+    }
+
+    return(
+        <ServiceContext.Provider value={values}>
+            {children}
+        </ServiceContext.Provider>
+    )
+}
+
+
+const useServiceContext = ()=>{
+    const context = useContext(ServiceContext);
+
+    if(context === undefined){
+        throw new Error('Context is not being used under its provider')
+    }
+
+    return context
+}
+
+export {useServiceContext, ServiceContextProvider }
