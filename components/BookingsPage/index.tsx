e34bf649c839f3744c597215bef22ac242ab199a
mujeex <mujahid.bappai@yahoo.com>
2022-11-14 11:09:02 +0100

    feat: add filter to orderStatus column of bookings

--- components/BookingsPage/index.tsx
+++ components/BookingsPage/index.tsx
@@ -3,41 +3,45 @@ import type { FilterConfirmProps } from 'antd/es/table/interface';
 import React, { useRef, useState } from 'react'
 import {Typography,Button,Avatar, Tag, InputRef, Input, Space} from 'antd'
 import { useRouter } from 'next/router'
-import Table, { ColumnsType, ColumnType } from 'antd/lib/table';
+import Table, { ColumnsType, ColumnType, TableProps } from 'antd/lib/table';
 import Highlighter from 'react-highlight-words'
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { FilterDropdownProps } from 'antd/lib/table/interface';
+import { FilterDropdownProps, FilterValue, SorterResult } from 'antd/lib/table/interface';
 
 
 
 const {Title,Text} = Typography
 
 
-interface DataType {
+interface Order {
     id: string;
     userId: string,
     name: string;
     ticketDate: string;
     quantity: number,
     orderStatus: string,
+    ticketStatus: string,
     price:number,
     total: number,
     uniqueCode: string,
+    userTicketId: string,
     paymentIntentStatus: string,
     orgServiceItemId: string,
   }
 
-  type DataIndex = keyof DataType;
+  type DataIndex = keyof Order;
 
-const bookings: DataType[] = [
+const bookings: Order[] = [
     {
       id: '1',
       userId:'mbappai',
       ticketDate:'Jan 22, 2022',
       name: 'Avery pro line skip',
       quantity: 4,
-      orderStatus: 'Redeemed',
+      ticketStatus: 'Redeemed',
+      orderStatus: 'Paid',
+      userTicketId:'dfadre364ikji',
       price: 2500,
       total:230,
       uniqueCode: '34u12y',
@@ -50,8 +54,10 @@ const bookings: DataType[] = [
       userId:'ommore',
       ticketDate:'Jan 12, 2022',
       quantity: 1,
-      orderStatus: 'Expired',
+      ticketStatus: 'Expired',
+      orderStatus: 'Initiated',
       price: 2500,
+      userTicketId:'dfadre364ikji',
       total:438,
       uniqueCode: '34u12y',
       paymentIntentStatus: 'PAYMENT_PAID',
@@ -63,7 +69,9 @@ const bookings: DataType[] = [
       userId:'schachindra',
       ticketDate:'Apr 22, 2022',
       quantity: 2,
-      orderStatus: 'Valid',
+      ticketStatus: 'Valid',
+      orderStatus: 'Paid',
+      userTicketId:'dfadre364ikji',
       price: 2500,
       total:438,
       uniqueCode: '34u12y',
@@ -92,6 +100,7 @@ export default function Bookings(){
   // );
 
   const [searchText, setSearchText] = useState('');
+  const [filteredInfo, setFilteredInfo] = useState<Record<string, FilterValue | null>>({});
   const [searchedColumn, setSearchedColumn] = useState('');
   const searchInput = useRef<InputRef>(null);
 
@@ -105,12 +114,21 @@ export default function Bookings(){
     setSearchedColumn(dataIndex);
   };
 
+  const clearFilters = () => {
+    setFilteredInfo({});
+  };
+
   const handleReset = (clearFilters: () => void) => {
     clearFilters();
     setSearchText('');
   };
 
-  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
+  const handleChange: TableProps<Order>['onChange'] = (pagination, filters, sorter) => {
+    console.log('Various parameters', pagination, filters, sorter);
+    setFilteredInfo(filters);
+  };
+
+  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<Order> => ({
     filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters}) => (
       <div style={{ padding: 8 }} onKeyDown={e => e.stopPropagation()}>
         <Input
@@ -188,23 +206,39 @@ export default function Bookings(){
   });
 
 
-  const columns: ColumnsType<any> = [
-    {
-      title: 'UserId',
-      dataIndex: 'userId',
-      key: 'userId',
-    //   render: (text,record) => <Link href={`/stores/${record.key}`}><a>{text}</a></Link>,
-    },
-    {
-      title: 'Unique Code',
-      dataIndex: 'uniqueCode',
-      key: 'uniqueCode',
-      ...getColumnSearchProps('uniqueCode'),
-    },
+  const columns: ColumnsType<Order> = [
     {
       title: 'Service',
       dataIndex: 'name',
       key: 'name',
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'UserId',
+      dataIndex: 'userId',
+      key: 'userId',
+      ...getColumnSearchProps('name'),
+    },
+    
+    {
+      title: 'Order Status',
+      dataIndex: 'orderStatus',
+      key: 'orderStatus',
+      render: orderStatus=>{
+        let color = 'blue'
+        if(orderStatus==='Paid') color='green'
+        if(orderStatus==='Initiated') color='blue'
+        return(
+          <Tag color={color}>{orderStatus}</Tag>
+        )
+      },
+      filters: [
+        { text: 'Paid', value: 'Paid' },
+        { text: 'Initiated', value: 'Initiated' },
+      ], 
+      filteredValue: filteredInfo.orderStatus || null,
+      //@ts-ignore
+      onFilter: (value: string, record) => record.orderStatus.includes(value),
     },
     {
       title: 'Unit Price',
@@ -233,19 +267,6 @@ export default function Bookings(){
     }
     },
     {
-      title: 'Order Status',
-      dataIndex: 'orderStatus',
-      key: 'orderStatus',
-      render: orderStatus=>{
-        let color = 'blue'
-        if(orderStatus==='Redeemed') color='green'
-        if(orderStatus==='Expired') color='red'
-        return(
-          <Tag color={color}>{orderStatus}</Tag>
-        )
-    }
-    },
-    {
       title: 'TicketDate',
       dataIndex: 'ticketDate',
       key: 'ticketDate',
@@ -255,7 +276,8 @@ export default function Bookings(){
 
     return(
       <div>
-        <Table style={{width:'100%'}} columns={columns} dataSource={bookings} />
+        <Button type='link' style={{marginBottom:'.5em'}} onClick={clearFilters}>Clear filters</Button>
+        <Table style={{width:'100%'}} columns={columns} onChange={handleChange} dataSource={bookings} />
       </div>
     )
 }
