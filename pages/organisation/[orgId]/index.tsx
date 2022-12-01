5b0e9f05a86b380446998ad8ffddc42090f96610
mujeex <mujahid.bappai@yahoo.com>
2022-12-01 21:37:17 +0100

    refactor: dynamically import servicepage

--- pages/organisation/[orgId]/index.tsx
+++ pages/organisation/[orgId]/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{Suspense} from 'react'
+import {Spin} from 'antd'
 import ServiceView from '../../../components/ServicesPage/ServicesView/ServicesView'
+import dynamic from 'next/dynamic'
+
+const DynamicServices = dynamic(()=>import('../../../components/ServicesPage/ServicesView/ServicesView'),{
+    ssr:false
+})
 
 export default function Services(){
 
     return( 
-         <ServiceView/>
+        <Suspense fallback={<Spin size='large'/>}>
+            <DynamicServices/>
+        </Suspense>
     )
 }
