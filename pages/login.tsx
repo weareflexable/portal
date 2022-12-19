0f6530734ae40249ac5c0145c3954fd251f89fd2
mujeex <mujahid.bappai@yahoo.com>
2022-12-19 09:57:45 +0100

    refactor: show spinner in login page while checking user auth

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import {Button, Typography, Card} from 'antd'
+import {Button, Typography, Card, Spin} from 'antd'
 import { useRouter } from 'next/router'
 import { useEffect,useState } from 'react'
 import { useAuthContext } from '../context/AuthContext';
@@ -23,33 +23,40 @@ export default function Login(){
     //     }
     // }, [push, query, setIsAuthenticated])
 
-    if(isAuthenticated){
-        replace('/')
-    }
-
     const handleLogin = ()=>{ 
         if(window !== undefined){
             location.href = `${process.env.NEXT_PUBLIC_AUTH}/login?redirect_to=portal`
         }
     }
 
+    useEffect(() => {
+      if(isAuthenticated){
+          replace('/')
+      }
+    }, [isAuthenticated, replace])
+
+
+    if(!isAuthenticated){
+        return <LoginView  handleLogin={handleLogin}/>
+    }
 
     return(
-        <div style={{width:'100vw',display:'flex', background:'#f9f9f9', justifyContent:'center',alignItems:'center',height:'100vh'}}>
-            <Card style={{width:'30%'}}>
-                {!isAuthenticated
-                    ?
-                    <>
-                        <Title level={5}>Login to portal</Title>
-                        <Button size='large' shape='round' type='primary' onClick={handleLogin}>Login</Button>
-                    </>
-                    :
-                    <>
-                    <Text type='secondary'>Logged in!</Text>   
-                    <Button danger  type='link' onClick={logout}>Logout</Button>
-                    </>
-                }
-            </Card>
+        <div style={{width:'100vw',minHeight:'100vh',background:'#f4f4f4',display:'flex',justifyContent:'center',alignItems:'center'}}>
+            <Spin size='large'/>
         </div>
-    )
+    ) 
+}
+
+interface LoginViewProps{
+    handleLogin:()=>void
+
+}
+function LoginView({handleLogin}: LoginViewProps) {
+    return <div style={{ width: '100vw', display: 'flex', background: '#f9f9f9', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Card style={{ width: '30%' }}>
+                    <Title level={5}>Login to portal</Title>
+                    <Button size='large' shape='round' type='primary' onClick={handleLogin}>Login</Button>
+             
+        </Card>
+    </div>;
 }
