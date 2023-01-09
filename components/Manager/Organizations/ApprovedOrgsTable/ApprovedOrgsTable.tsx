2e6520a6e9f0f74d8acff6e6170b9ba58774cfe7
mujeex <mujahid.bappai@yahoo.com>
2023-01-09 21:16:37 +0100

    feat: accept and reject org

--- components/Manager/Organizations/ApprovedOrgsTable/ApprovedOrgsTable.tsx
+++ components/Manager/Organizations/ApprovedOrgsTable/ApprovedOrgsTable.tsx
@@ -1,12 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Card, List, Button, Avatar, Typography, Tag } from "antd";
 import axios from "axios";
+import {PlusCircleOutlined} from '@ant-design/icons'
 import { useAuthContext } from "../../../../context/AuthContext";
+import { useState } from "react";
 
 
 export default function ApprovedOrgs(){
 
     const {paseto} = useAuthContext()
+    const queryClient = useQueryClient()
+
+    const [selectedOrg, setSelelectedOrg] = useState(null)
 
     async function fetchApprovedOrgs(){
     const res = await axios({
@@ -14,9 +19,9 @@ export default function ApprovedOrgs(){
             url:`${process.env.NEXT_PUBLIC_NEW_API_URL}/manager/orgs`,
             data:{
                 key:'status',
-                value: '2',
+                value: '1',
                 page_number: 0,
-                page_size: 3
+                page_size: 6
             },
             headers:{
                 "Authorization": paseto
@@ -25,34 +30,96 @@ export default function ApprovedOrgs(){
         return res.data;
     }
 
-    const orgQuery = useQuery({queryKey:['approvedOrgs'], queryFn:fetchApprovedOrgs, enabled:paseto !== ''})
+    async function acceptOrg(orgId: string){
+        const res = await axios({
+            method:'patch',
+            url:`${process.env.NEXT_PUBLIC_NEW_API_URL}/manager/org`,
+            data:{
+                key:'status',
+                value: '2',
+                org_id: orgId
+            },
+            headers:{
+                "Authorization": paseto
+            }
+        })
+        return res; 
+    }
 
-    console.log(orgQuery.data)
+    async function rejectOrg(orgId:string){
+        const res = await axios({
+            method:'patch',
+            url:`${process.env.NEXT_PUBLIC_NEW_API_URL}/manager/org`,
+            data:{
+                key:'status',
+                value: '4',
+                org_id: orgId
+            },
+            headers:{
+                "Authorization": paseto
+            }
+        })
+        return res; 
+    }
+
+    const acceptOrgMutation = useMutation(['acceptOrgMutation'],{
+        mutationFn: acceptOrg,
+        onSuccess:(data:any)=>{
+            console.log('successfully mutate data', data)
+            queryClient.invalidateQueries({queryKey:['inReviewOrgs']})
+        },
+        onError:()=>{
+            console.log('Error changing status')
+        }
+    })
+
+    const rejectOrgMutation = useMutation(['rejectOrgMutation'],{
+        mutationFn: rejectOrg,
+        onSuccess:(data:any)=>{
+            console.log('successfully mutate data', data)
+            queryClient.invalidateQueries({queryKey:['inReviewOrgs']})
+        },
+        onError:()=>{
+            console.log('Error changing status')
+        }
+    })
+
+    function rejectOrgHandler(org:any){
+        setSelelectedOrg(org.org_id)
+        rejectOrgMutation.mutate(org.org_id)
+
+    }
+    function acceptOrgHandler(org:any){
+        setSelelectedOrg(org.org_id)
+        acceptOrgMutation.mutate(org.org_id)
+    }
+
+    const orgQuery = useQuery({queryKey:['inReviewOrgs'], queryFn:fetchApprovedOrgs, enabled:paseto !== ''})
+    const approvedOrgs = orgQuery.data && orgQuery.data.data
+
+
 
         return (
-            <div>
-                Approved orgs here
-            </div>
-            // // <Card style={{ width: '100%', marginTop: '1em' }}>
-            // {/* <Button disabled={isLoadingOrgs} type='link' style={{ marginBottom: '1em', display: 'flex', alignItems: 'center' }} icon={<PlusCircleOutlined />} onClick={onShowCreateOrgForm}>Register new organisation</Button> */}
-            // {/* <List
-            //     size="small"
-            //     bordered={false}
-            //     // loading={isLoadingOrgs}
-            //     dataSource={uniqueOrgs}
-            //     renderItem={item => <List.Item
-            //         actions={[<Button key={item.id} shape='round' disabled={!item.approved} type='primary' loading={item.id === selectedOrg ? isNavigatingToOrgs : false} onClick={() => navigateToApp(item)}>{item.approved ? `Go to organization` : 'Organization in review'}</Button>]}
-            //         style={{ border: 'none', backgroundColor: '#f9f9f9', marginBottom: '.5em', padding: '1em', borderRadius: '4px' }}
-            //         key={item.id}
-            //     >
-            //         <List.Item.Meta
-            //             avatar={<Avatar src={`https://nftstorage.link/ipfs/${item.imageHash}`} />}
-            //             title={<div style={{ display: 'flex' }}>
-            //                 <Typography.Text style={{ marginRight: '1em' }}>{item.name}</Typography.Text>
-            //                 <Tag>{item.role === 'STAFF' ? 'Employee' : item.role}</Tag>
-            //             </div>} />
 
-            //     </List.Item>} />
-            // </Card> */}
+            <Card style={{ width: '100%', marginTop: '1em' }}>
+             <List
+                size="small"
+                bordered={false}
+                loading={orgQuery.isLoading}
+                dataSource={approvedOrgs}
+                renderItem={(item:any )=> <List.Item
+                    actions={[<Button key={item.id} shape='round' loading={selectedOrg === item.org_id && rejectOrgMutation.isLoading}  onClick={()=>rejectOrgHandler(item)} danger type="text" >Reject Request</Button>,<Button key={item.id} shape='round' loading={selectedOrg === item.org_id && acceptOrgMutation.isLoading} onClick={()=>acceptOrgHandler(item)} type='primary' >Accept Request</Button>]}
+                    style={{ border: 'none', backgroundColor: '#f9f9f9', marginBottom: '.5em', padding: '1em', borderRadius: '4px' }}
+                    key={item.id}
+                >
+                    <List.Item.Meta
+                        avatar={<Avatar src={`https://nftstorage.link/ipfs/${item.imageHash}`} />}
+                        title={<div style={{ display: 'flex' }}>
+                            <Typography.Text style={{ marginRight: '1em' }}>{item.name}</Typography.Text>
+                            {/* <Tag>{item.role === 'STAFF' ? 'Employee' : item.role}</Tag> */}
+                        </div>} />
+
+                </List.Item>} />
+            </Card>
     )
 }
