ffd37e7ff9988804fbd505cd0fe52dd138f04276
mujeex <mujahid.bappai@yahoo.com>
2023-01-31 11:38:23 +0100

    feat: move billings to service page

--- pages/organizations/services/billings/index.tsx
+++ pages/organizations/services/billings/index.tsx
@@ -1,31 +1,39 @@
 import { Content } from 'antd/lib/layout/layout'
-import { Typography, Row, Col } from 'antd'
+import { Typography, Row, Button, Col } from 'antd'
 const {Title} = Typography
+import {ArrowLeftOutlined} from '@ant-design/icons'
 import React from 'react'
 import BillingsView from '../../../../components/BillingsPage'
 import AppLayout from '../../../../components/shared/Layout/layout'
+import { useRouter } from 'next/router'
 
 export default function Billings(){
+
+    const router = useRouter()
+    
     return(
-        <AppLayout>
-             <Row>
+        // <AppLayout>
+        <div style={{background:'#f6f6f6', height:'100%', minHeight:'100vh'}}>
+            <Row>
                 <Col offset={1} span={22}>
-                    <Title style={{marginLeft: '1em', marginTop:'1em'}} level={3}>Billings</Title>
+                    <div style={{display:'flex', flex:'7', marginTop:'1rem', marginBottom:'2rem', flexDirection:'column'}}> 
+                        <Button style={{display:'flex', padding: '0', margin:'0', alignItems:'center', textAlign:'left'}} onClick={()=>router.back()} icon={<ArrowLeftOutlined />} type='link'>Back to services</Button>
+                        <Title style={{margin:0}} level={2}> Billings </Title>    
+                    </div>
+                </Col> 
+            </Row>
+             <Row >
+                <Col offset={1} span={22}>
                     <Content
                         style={{
-                        // padding: '1em',
-                        margin:'1em',
-                        // background:'#f7f7f7' ,
-                        // width:`98%`,
-                        maxWidth:'100%',
-                        // height: '100%',
-                        // minHeight:'70vh',
+                            // margin:'1em',
+                            maxWidth:'100%',
                         }}
-                    >
+                        >
                         <BillingsView/>
                     </Content>
                 </Col>
             </Row>
-    </AppLayout> 
+    </div>
     )
 }
