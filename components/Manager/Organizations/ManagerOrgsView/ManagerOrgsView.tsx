cc24dc6417741deab0316f6fa446e6c30479ebba
mujeex <mujahid.bappai@yahoo.com>
2023-02-04 19:37:09 +0100

    fix: remove elements that use data from local storgae

--- components/Manager/Organizations/ManagerOrgsView/ManagerOrgsView.tsx
+++ components/Manager/Organizations/ManagerOrgsView/ManagerOrgsView.tsx
@@ -367,7 +367,7 @@ export default function ManagerOrgsView(){
         render:(_,record)=>{
             return(
                 <div style={{display:'flex',alignItems:'center'}}>
-                    <Image style={{width:'30px', height: '30px', marginRight:'.8rem', borderRadius:'50px'}} alt='Organization logo' src={'/favicon.ico'}/>
+                    <Image style={{width:'30px', height: '30px', marginRight:'.8rem', borderRadius:'50px'}} alt='Organization logo' src={`${process.env.NEXT_PUBLIC_NFT_STORAGE_PREFIX_URL}/${record.logoImageHash}`}/>
                     <div style={{display:'flex',flexDirection:'column'}}>
                        { record.status !==1?<Text>{record.name}</Text>:<Text style={{color:'#1677ff', cursor:'pointer'}} onClick={()=>gotoServices(record)}>{record.name}</Text> }   
                         <Text type="secondary">{record.email}</Text>
