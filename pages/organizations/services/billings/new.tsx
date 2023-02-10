29f37f1949197f49223af3ca372bea26ebedd767
mujeex <mujahid.bappai@yahoo.com>
2023-02-10 21:52:56 +0100

    style: group forms into sections

--- pages/organizations/services/billings/new.tsx
+++ pages/organizations/services/billings/new.tsx
@@ -1,7 +1,7 @@
 
 
 import React,{useState} from 'react'
-import {Card,Form,Input, InputNumber,Button, Typography, Radio, notification, Row, Col} from 'antd'
+import {Card,Form,Input, InputNumber,Button, Typography, Radio, notification, Row, Col, Segmented, Checkbox} from 'antd'
 import { Bank } from '../../../../components/BillingsPage/Types/Banks.types'
 import { useOrgContext } from '../../../../context/OrgContext'
 import {ArrowLeftOutlined} from '@ant-design/icons'
@@ -75,15 +75,15 @@ export default function CreateBankAccountForm(){
                 <Row>
                     <Col offset={1}> 
                         <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-                            <Button shape='round' style={{marginRight:'1rem'}} type='text' onClick={()=>router.back()} icon={<ArrowLeftOutlined/>}/>
-                            <Title style={{margin:'0'}} level={3}>Create new bank account</Title>
+                            <Button shape='round' style={{marginRight:'.2rem'}} type='link' onClick={()=>router.back()} icon={<ArrowLeftOutlined/>}/>
+                            <Title style={{margin:'0'}} level={3}>Add Bank Details</Title>
                         </div>
                     </Col>
                 </Row>
             </div>
             
             <Row>
-                <Col offset={5} span={10}>
+                <Col offset={2} span={10}>
                 <Form
                     name="billingsForm"
                     initialValues={{ remember: true }}
@@ -92,48 +92,74 @@ export default function CreateBankAccountForm(){
                     form={form}
                 >
                 <div style={{marginBottom:'2rem'}}>
-                <Title level={3}>Beneficiary info</Title>
+                <Title level={3}>Beneficiary details</Title>
                 </div>
 
-                <Form.Item
-                    name="beneficiaryName"
-                    label='Name'
-                    rules={[{ required: true, message: 'Please input name used on card' }]}
-                >
-                    <Input size='large' placeholder="Bill Cage" />
-                </Form.Item>
-
-                <Form.Item
-                    name="beneficiaryAddress"
-                    label='Address'
-                    rules={[{ required: true, message: 'Please enter valid address' }]}
-                >
-                    <Input size='large' placeholder="89, Highstreet Boston" />
-                </Form.Item>
-
-
-                <Form.Item
-                    name="currency"
-                    label="Currency"
-                    rules={[{ required: true, message: 'Please select your currency' }]}
-                >
-                    <Input placeholder="USD" />
-                </Form.Item>
-
-                <Form.Item 
-                    label="Account type" 
-                    name="accountType"
-                    rules={[{ required: true, message: 'Please select an accountType' }]}
+                <div style={{border:'1px solid #e2e2e2', borderRadius:'4px', padding:'1rem'}}>    
+                    <Form.Item
+                        name="beneficiaryName"
+                        label='Name'
+                        rules={[{ required: true, message: 'Please input name used on card' }]}
                     >
-                    <Radio.Group>
-                        <Radio.Button value="savings">Savings</Radio.Button>
-                        <Radio.Button value="current">Current</Radio.Button>
-                    </Radio.Group>
-                </Form.Item>
+                        <Input size='large' placeholder="Bill Cage" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="beneficiaryAddress"
+                        label='Address'
+                        style={{marginBottom:'0'}}
+                        rules={[{ required: true, message: 'Please enter valid address' }]}
+                    >
+                        <Input size='large' placeholder="89, Highstreet Boston" />
+                    </Form.Item>
+
+                </div>
+
 
                 <div style={{marginBottom:'2rem', marginTop:'3rem'}}>
-                <Title level={3}>Bank info</Title>
+                <Title level={3}>Account details</Title>
                 </div>
+
+                <div style={{border:'1px solid #e2e2e2', borderRadius:'4px', padding:'1rem'}}> 
+
+                    <Form.Item 
+                        label="Account type" 
+                        name="accountType"
+                        rules={[{ required: true, message: 'Please select an accountType' }]}
+                        >
+                        <Radio.Group size='large'>
+                            <Radio.Button value="savings">Savings</Radio.Button>
+                            <Radio.Button value="current">Current</Radio.Button>
+                        </Radio.Group>
+                    </Form.Item>
+
+                    <Form.Item
+                        name="accountNo"
+                        label='Account number'
+                        rules={[{ required: true, message: 'Please input a valid account number' }]}
+                    >
+                        <Input allowClear size='large' placeholder="0127467382" />
+                    </Form.Item>
+
+            
+                    <Form.Item
+                        name="currency"
+                        label="Currency"
+                        style={{marginBottom:'0'}} 
+                        rules={[{ required: true, message: 'Please select your currency' }]}
+                    >
+                        <Input size='large' placeholder="USD" />
+                    </Form.Item>
+                </div>
+
+
+
+                <div style={{marginBottom:'2rem', marginTop:'3rem'}}>
+                <Title level={3}>Bank details</Title>
+                </div>
+
+
+                <div style={{border:'1px solid #e2e2e2', borderRadius:'4px', padding:'1rem'}}>  
 
                 <Form.Item
                     name="bankName"
@@ -143,13 +169,6 @@ export default function CreateBankAccountForm(){
                     <Input size='large' placeholder="Silvergate crest bank" />
                 </Form.Item>
 
-                <Form.Item
-                    name="accountNo"
-                    label='Account number'
-                    rules={[{ required: true, message: 'Please input a valid account number' }]}
-                >
-                    <Input allowClear size='large' placeholder="0127467382" />
-                </Form.Item>
 
                 <Form.Item
                     name="bankAddress"
@@ -160,27 +179,41 @@ export default function CreateBankAccountForm(){
                 </Form.Item>
 
                 <Form.Item
-                    name="routingNumber"
-                    label='Routing number'
-                    rules={[{ required: true, message: 'Please input a valid routing number' }]}
-                >
-                    <Input size='large' placeholder="433354564" />
-                </Form.Item>
-
-                <Form.Item
                     name="swiftCode"
                     label='Swift/BIC code'
                     rules={[{ required: true, message: 'Please input a valid routing number!' }]}
                 >
-                    <Input size='large' type='number' placeholder="37567489374" />
+                    <Input size='large' placeholder="AAAA-BB-CC-123" />
+                
                 </Form.Item>
 
+                <Form.Item
+                    name="routingNumber"
+                    label='Routing number'
+                    rules={[{ required: true, message: 'Please input a valid routing number' }]}
+                >
+                    <Input size='large' placeholder="623852453 1234567 001" />
+                </Form.Item>
+
+                </div>
+
+                    {/* <Form.Item
+                        name="currency"
+                        label="Currency"
+                        style={{marginBottom:'0'}} 
+                        rules={[{ required: true, message: 'Please select your currency' }]}
+                    >
+                        <Checkbox>
+                            I have read the terms and conditions and I am fully aware to the consequences of my actions
+                        </Checkbox>
+                    </Form.Item> */}
 
                 <Form.Item style={{marginTop:'3rem'}}>
                     <Button type="primary" size='large' shape='round' loading={isCreatingData} htmlType="submit">
-                    { isCreatingData? 'Creating...' :'Add bank details'}
+                    { isCreatingData? 'Creating...' :'Add bank'}
                     </Button>
                 </Form.Item>
+                
 
             </Form> 
                 </Col>
