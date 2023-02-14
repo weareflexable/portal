c0d1e667f57623fc46d7e82b9d1c2eca66922236
mujeex <mujahid.bappai@yahoo.com>
2023-02-14 14:43:26 +0100

    feat: create scaffold for platform page

--- components/Manager/Platform/index.tsx
+++ components/Manager/Platform/index.tsx
@@ -1,7 +1,53 @@
-import { Button } from "antd";
+import { Button, Tabs} from "antd";
+import type { TabsProps } from 'antd';
+import { useState } from "react";
 
 export default function PlatformView(){
+
+    const [currentTab, setCurrentTab] = useState("1") 
+
+    const items: TabsProps['items'] = [
+        {
+          key: '1',
+          label: `Service types`,
+          children: <ServiceTypesView/>,
+        },
+        {
+          key: '2',
+          label: `Service-item types`,
+          children: <ServiceItemTypesView/>,
+        },
+      ];
+
+    function onChangeTab(key:string){
+        setCurrentTab(key)
+        console.log(key)
+    }
+
     return(
-        <Button>Create new service type</Button>
+        <Tabs defaultActiveKey={currentTab} items={items} onChange={onChangeTab} />
+        )
+    }
+    
+    
+    
+    
+function ServiceTypesView(){
+        console.log('rendering servicetypes')
+        return(
+            <div>
+            List of service types and actions
+            <Button>Create new service type</Button>
+        </div>
+    )
+}
+
+function ServiceItemTypesView(){
+    console.log('rendering serviceItemtypes')
+    return(
+        <div>
+            List of service item types and actions
+            <Button>Create new service type</Button>
+        </div>
     )
 }
