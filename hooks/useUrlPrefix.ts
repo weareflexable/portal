63860db161eb91be91ce4e2d4dd150e39c95d755
mujeex <mujahid.bappai@yahoo.com>
2023-03-11 07:28:08 +0100

    typo: correct casing for super-admin urlPrefix

--- hooks/useUrlPrefix.ts
+++ hooks/useUrlPrefix.ts
@@ -5,6 +5,6 @@ export default function useUrlPrefix(){
     const {currentUser} =  useAuthContext()
     const {isAdmin, isManager, isSuperAdmin } = useRole()
 
-    const urlPrefix = isManager ? 'manager': isAdmin?'admin': isSuperAdmin?'superadmin': 'users'
+    const urlPrefix = isManager ? 'manager': isAdmin?'admin': isSuperAdmin?'super-admin': 'users'
     return urlPrefix;
 } 
