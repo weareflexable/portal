4f15ba3472adcac7ebd368fd47ac2b2e7ea3160f
mbappai <mujahid.bappai@yahoo.com>
2023-03-12 23:54:24 +0100

    refactor: hide service switcher from manager and superadmin

--- components/Header/ServicesSwitcherButton/ServicesSwitcherButton.tsx
+++ components/Header/ServicesSwitcherButton/ServicesSwitcherButton.tsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import {Typography,Avatar,Button} from 'antd'
 import { useServicesContext } from '../../../context/ServicesContext';
 import { useAuthContext } from '../../../context/AuthContext';
+import useRole from '../../../hooks/useRole';
 const {Text,Title,Paragraph} = Typography;
 
 interface ServiceSwitcherProps{
@@ -12,6 +13,7 @@ export default function ServiceSwitcher({onOpenSwitcher}:ServiceSwitcherProps){
 
   const {currentService} =  useServicesContext()
   const {currentUser} = useAuthContext()
+  const {isManager, isSuperAdmin} = useRole()
     
     return (
                 <div
@@ -26,7 +28,7 @@ export default function ServiceSwitcher({onOpenSwitcher}:ServiceSwitcherProps){
                   <Avatar src={`https://nftstorage.link/ipfs/${currentService.logoImageHash}`}/>
                   <div style={{display:'flex', flexDirection:'column'}}>
                     <Text  ellipsis  style={{marginBottom:'.001em', width:'150px', marginRight:'.5em', marginLeft:'.5em'}}>{currentService.name}</Text>
-                    {currentUser.role == 1? null : <Button onClick={onOpenSwitcher} type='link' size='small' style={{padding:'0', textAlign:'start', margin:'0', marginLeft:'.5em'}}>Switch service</Button>}
+                    {isSuperAdmin || isManager ? null : <Button onClick={onOpenSwitcher} type='link' size='small' style={{padding:'0', textAlign:'start', margin:'0', marginLeft:'.5em'}}>Switch service</Button>}
                   </div>
                 </div>
           )
