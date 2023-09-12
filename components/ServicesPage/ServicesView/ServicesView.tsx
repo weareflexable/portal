75e0f7f344360dd8e08d848930fd8638d8398d35
mbappai <mujahid.bappai@yahoo.com>
2023-09-12 07:43:33 +0100

    refactor: rename service-items to exclusive-access

--- components/ServicesPage/ServicesView/ServicesView.tsx
+++ components/ServicesPage/ServicesView/ServicesView.tsx
@@ -146,7 +146,7 @@ function VenuesTable(){
 function gotoServiceItemsPage(service:Service){
   switchService(service)
   // navigate user to services page
-  router.push('/organizations/venues/serviceItems') // redirect to dashboard later
+  router.push('/organizations/venues/exclusive-access') // redirect to dashboard later
 }
 
 
@@ -369,7 +369,7 @@ function gotoServices(service:Service){
   // switch org 
   switchService(service)
   // navigate user to services page
-  router.push('/organizations/venues/serviceItems') // redirect to dashboard later
+  router.push('/organizations/venues/exclusive-access') // redirect to dashboard later
 }
 
 function toggleDeleteModal(){
