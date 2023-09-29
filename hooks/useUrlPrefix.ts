0f7bbea9787ac36d1de1c98dfc77a3611ca30f16
mbappai <mujahid.bappai@yahoo.com>
2023-09-29 16:24:14 +0100

    refactor: fix superadmin typo

--- hooks/useUrlPrefix.ts
+++ hooks/useUrlPrefix.ts
@@ -4,6 +4,6 @@ import useRole from "./useRole";
 export default function useUrlPrefix(){
     const {isAdmin, isManager, isSuperAdmin } = useRole()
 
-    const urlPrefix = isManager ? 'manager': isAdmin?'admin': isSuperAdmin?'super-admin': 'user'
+    const urlPrefix = isManager ? 'manager': isAdmin?'admin': isSuperAdmin?'superadmin': 'user'
     return urlPrefix;
 } 
