c107e8fbdeb8c98195ae0535953070baf2ac8272
mbappai <mujahid.bappai@yahoo.com>
2023-10-03 04:38:59 +0100

    choir: set copy link for staging

--- pages/organizations/events/index.tsx
+++ pages/organizations/events/index.tsx
@@ -453,7 +453,7 @@ const{isLoading:isDeletingItem} = deleteData
 function copyLink(selectedRecord:any){
   navigator.clipboard.writeText('')
   const eventId = selectedRecord.id
-  const marketplaceLink = `https://marketplace.dev.flexabledats.com/events/${eventId}`
+  const marketplaceLink = `https://marketplace.staging.flexabledats.com/events/${eventId}`
    // Copy the text inside the text field
    navigator.clipboard.writeText(marketplaceLink);
 }
